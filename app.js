9fb18f084db1f9952e373c4f7956f36c7f483646
kimwoojin <kimwoojin1903@gmail.com>
2023-10-16 16:02:57 +0900

    html 서빙 추가

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ const server = http.createServer((req, res) => {
         res.writeHead(500);
         return res.end('서버오류');
     }
+
+    if (req.url === '/' && req.method === 'GET') {
+        fs.readFile('index.html', 'utf8', (err, data) => {
+            if (err) {
+                serverErrorLog();
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    }
 });
