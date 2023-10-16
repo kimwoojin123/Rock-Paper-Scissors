343e5070f8a0452a29c7cb3b42028157de9513f3
kimwoojin <kimwoojin1903@gmail.com>
2023-10-16 16:00:30 +0900

    서버 에러함수 작성

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    function serverErrorLog() {
+        res.writeHead(500);
+        return res.end('서버오류');
+    }
+});
