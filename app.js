b0b8a845e77a22a23b5c060a6f8840d15f6c2a89
kimwoojin <kimwoojin1903@gmail.com>
2023-10-17 11:28:26 +0900

    서버 수정사항

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const http = require('http');
 const fs = require('fs');
-const url = require(`url`);
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    let parsedURL = url.parse(req.url, true);
     function serverErrorLog() {
         res.writeHead(500);
         return res.end('서버오류');
@@ -33,26 +32,27 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, { 'Content-Type': 'aplication/javascript' });
             res.end(data);
         });
-    } else if (parsedURL === '/win.html' && req.method === 'GET') {
+    } else if (req.url === '/win.html' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         fs.readFile('win.html', 'utf8', (err, data) => {
             if (err) {
                 serverErrorLog();
             }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(data);
         });
-    } else if (parsedURL === '/lose.html' && req.method === 'GET') {
+    } else if (req.url === '/lose.html' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         fs.readFile('lose.html', 'utf8', (err, data) => {
             if (err) {
                 serverErrorLog();
             }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(data);
         });
     } else {
         res.writeHead(404);
         res.end('서빙 오류');
     }
+    console.log(req.url);
 });
 
 const PORT = 8000;
